23cdc1df67cd1dfbbf00d23d2f6f8607530daa0c
Abd abuelhija <a.abd.ab@hotmail.com>
2025-05-28 18:56:45 +0300

    Updated files

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from routes import players, admins
+import ssl, sys
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"welcome to sport project backend"}
+    return {"message": "welcome to sport project backend"}
+
+@app.get("/versions")
+def versions():
+    return {
+        "python_version": sys.version,
+        "openssl_version": ssl.OPENSSL_VERSION,
+    }
 
 # Include routers
 app.include_router(players.router)
